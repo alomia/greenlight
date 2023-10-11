a36e4596c3fda7ca3cd4933f9dcfba535ea55e9e
Alejandro Alomia <alejandroalomia730@gmail.com>
2023-10-11 12:19:51 -0500

    custom JSON decoding

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInavalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -13,4 +17,25 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInavalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInavalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInavalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
