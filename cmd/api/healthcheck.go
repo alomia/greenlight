058d9e75b6c5b8d402b6929724b294886086e5aa
Alejandro Alomia <alejandroalomia730@gmail.com>
2024-01-11 19:23:01 -0500

    json encoding

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-
-	w.Header().Set("Content-Type", "application/json")
-
-	fmt.Fprintln(w, js)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	err := app.writeJSON(w, data, http.StatusOK, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
